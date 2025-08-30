ba880baba38fe8af2992b110a19f444864ddc5e3
michalfica <michalfica125@gmail.com>
2025-08-30 15:57:22 +0200

    .sh script  updated

--- apply-learnlib-patch.sh
+++ apply-learnlib-patch.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-# Ensure submodules are up to date
+# Ensure submodules are up to f135932 commit 
 git submodule update --init --recursive
+cd learnlib
+git reset --hard f135932
+cd ../
 
 # Apply the patch
 echo "Applying LearnLib customizations..."
-patch -p1 -d learnlib < patches/learnlib-custom.patch
+patch -p1 -d learnlib < patches/file.patch
 
 echo "LearnLib has been patched successfully."
-
